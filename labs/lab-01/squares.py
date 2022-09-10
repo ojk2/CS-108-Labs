056a3597dafc40d4e3507c19a10254a0aab7cd4d
ojk2 <112818702+ojk2@users.noreply.github.com>
2022-09-10 16:12:57 -0400

    Add files via upload

---
+++ labs/lab-01/squares.py
@@ -0,0 +1,15 @@
+"""CS 108 - Lab 1.1
+
+This module will attempt to take a number
+as input and square it.
+
+@author: OWEN KOH (2635799)
+@author: YEWON JEON (10002120)
+@date: Fall, 2022
+"""
+
+num = int(input("Number: "))
+
+squared = num ** 2
+
+print(f"{num} squared is {squared}")
