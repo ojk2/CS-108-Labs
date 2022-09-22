2fb0796d4ae5719a9d40bdabf334512fccc0b500
ojk2 <112818702+ojk2@users.noreply.github.com>
2022-09-22 11:59:04 -0400

    Update wind_chill.py

--- labs/lab-04/wind_chill.py
+++ labs/lab-04/wind_chill.py
@@ -2,8 +2,11 @@
 
 This program will attempt to take two inputs,
 temperature and wind speed to calculate
-the wind chill using a formula. Branches are
-implemented to handle exceptions.
+the wind chill using a formula and to determine
+how many layers you should wear. 
+
+Branches are
+implemented to handle conditions and exceptions.
 
 @author: OWEN KOH (ojk2)
 @date: Fall, 2022
@@ -11,18 +14,24 @@ implemented to handle exceptions.
 
 # Put your solution code here, replacing this line.
 
+# take the inputs even as integers
 a = input("Temperature: ")
 b = input("Wind speed: ")
 
+# convert inputs to floats if they are integers
 ta = float(a)
 v = float(b)
+
+# main formula
 twc = 35.74 + 0.6215 * ta - 35.75 * (v ** 0.16) + 0.4275 * ta * (v ** 0.16)
 
+# error message first priority if calculated value is invalid in the first place
 if v < 2 or ta < -58 or ta > 41:
     print("Invalid input")
 else:
     print(f"Wind chill: {twc}")
-
+    
+    # conditions
     if twc < -40:
         print("Stay home!")
     elif twc < -10:
