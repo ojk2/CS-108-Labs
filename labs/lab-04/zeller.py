ed7deaa43cfdae371f9fa34e9e2d808c476da2eb
ojk2 <112818702+ojk2@users.noreply.github.com>
2022-09-22 14:59:10 -0400

    Update zeller.py

--- labs/lab-04/zeller.py
+++ labs/lab-04/zeller.py
@@ -8,12 +8,17 @@ inputs: year, month, and day using a formula.
 @date: Fall, 2022
 """
 
+# load in the days of the week in a list
 wk = ['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
 
+'''get the inputs as integers because there is no such thing as
+float day, month, or year.'''
 y = int(input("Enter year: "))
 m = int(input("Enter month: "))
-d = int(input("Enter day: "))
+q = int(input("Enter day: "))
 
+'''change up the inputs for certain exceptions so that the
+formula will work.'''
 if m == 1:
     m = 13
     y -= 1
@@ -21,8 +26,12 @@ elif m == 2:
     m = 14
     y -= 1
 
+# set constants to simplify formula a bit
 j = y // 100
 k = y % 100
 
-h = (m + (((d + 1) * 26) // 10) + k + (k // 4) + (j // 4) + (5 * j)) % 7
+# formula to calculate day of week given day, month, year
+h = (q + (((m + 1) * 26) // 10) + k + (k // 4) + (j // 4) + 5 * j) % 7
+
+# the result has to be in ranges 1-7 -> 0-6 range index list
 print(wk[h])
