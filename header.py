be77836040063d28a081abbe8616c5c69cbc5ae7
ojk2 <112818702+ojk2@users.noreply.github.com>
2022-09-23 13:48:07 -0400

    Update header.py

--- header.py
+++ header.py
@@ -1,11 +1,27 @@
 from sys import argv
 
-script, targetFile = argv
+script, targetFile, labhw = argv
 
 openFile = open(targetFile).read()
 
-
-print("Now enter a short description for the program.")
+print("Now enter a short description for the program.\n")
 
 while True:
   input()
+  
+
+
+  
+  
+  
+author1 = input("Enter name:: ")
+author1id = input("Enter id/email:: ")
+prompt2 = input("Add 2nd partner?? y/n :: ")
+
+if prompt == 'y':
+  author2 = input("Enter name:: ")
+  author2id = input("Enter id/email:: ")
+else:
+
+  
+date = input("Enter semester, year:: ")
