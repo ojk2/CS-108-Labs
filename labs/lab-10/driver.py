e2c41a1cc9b92466d4bcb17e021145830e7b02b4
ojk2 <112818702+ojk2@users.noreply.github.com>
2022-11-03 11:39:30 -0400

    Create driver.py

---
+++ labs/lab-10/driver.py
@@ -0,0 +1,35 @@
+"""CS 108 Lab 10
+
+This driver uses the Employee class to compute and save corporate statistics.
+
+@author: OWEN KOH (ojk2)
+@date: Fall, 2022
+"""
+
+from employee import Employee
+
+employees = []
+
+# Construct an employee object for each employee specified in 'employees.txt'
+# and add it to the employees list.
+# YOUR CODE HERE
+targetfile = open('employees.txt')
+
+for lines in targetfile.readlines():
+    values = lines.split(',')
+    person = Employee(values[0], values[1], values[2], int(values[3]))
+    employees.append(person)
+targetfile.close()
+
+
+
+# Write the total number of employees into the 'employee-count.txt' file.
+# YOUR CODE HERE
+newfile = open('employee-count.txt', 'w')
+newfile.write(str(len(employees)))
+newfile.close()
+
+# Compute and print out statistics for employees
+#print(employees) # YOU WILL REPLACE THIS LINE.
+for employee in employees:
+    print(employee)
